8ddfc2f3b055b0f683b02609b1c717f95e26e802
Nirjhar <choudhurynirjhar@gmail.com>
2019-09-30 20:34:43 -0400

    First checkin

---
+++ Splunk.Demo/ContainerConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+using System;
+
+
+namespace Splunk.Demo
+{
+    internal static class ContainerConfiguration
+    {
+        public static IServiceProvider Configure()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            LogManager.LoadConfiguration("nlog.xml");
+
+            serviceCollection.AddLogging(l => 
+            {
+                l.AddNLog();
+            }).Configure<LoggerFilterOptions>(c => c.MinLevel = Microsoft.Extensions.Logging.LogLevel.Trace);
+
+            return serviceCollection.BuildServiceProvider();
+        }
+    }
+}
---
+++ Splunk.Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Splunk.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = ContainerConfiguration.Configure();
+
+            var logger = serviceProvider.GetService<ILogger<Program>>();
+            logger.LogInformation($"Test splunk log {DateTime.Now}");
+        }
+    }
+}
